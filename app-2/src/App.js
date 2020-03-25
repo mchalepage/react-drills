638c421a42d3337c9d5f2760eb292596d1e77c76
mchalepage <me@mchale.page>
2020-03-25 15:38:13 -0600

    progress

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      cheeses: ['havarti', 'jarlsberg', 'gruyère', 'brie', 'provolone', 'smoked cheddar', 'irish cheddar', 'sharp cheddar', 'mozzarella']
+    }
+  }
+
+
+  render () {
+    let cheeseList = this.state.cheeses.map((element, index) => {
+    return <h2 key={index}>{element}</h2>
+    })
+    return (
+      <div className="App">
+        {cheeseList}
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
